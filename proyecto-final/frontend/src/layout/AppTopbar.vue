f792855763ac425048acff1149c666af45f1a99f
RandaeGS <garciasanchezrd@gmail.com>
2025-08-09 00:15:20 -0400

    clean frontend code and ui

--- proyecto-final/frontend/src/layout/AppTopbar.vue
+++ proyecto-final/frontend/src/layout/AppTopbar.vue
@@ -71,10 +71,6 @@ const logout = () => {
             <div class="layout-topbar-menu hidden lg:block">
                 <div class="layout-topbar-menu-content">
                     <button type="button" class="layout-topbar-action">
-                        <i class="pi pi-inbox"></i>
-                        <span>Messages</span>
-                    </button>
-                    <button type="button" class="layout-topbar-action">
                         <i class="pi pi-user"></i>
                         <span>Profile</span>
                     </button>
--- proyecto-final/frontend/src/views/pages/Landing.vue
+++ proyecto-final/frontend/src/views/pages/Landing.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import FeaturesWidget from '@/components/landing/FeaturesWidget.vue';
 import FooterWidget from '@/components/landing/FooterWidget.vue';
 import HeroWidget from '@/components/landing/HeroWidget.vue';
-import HighlightsWidget from '@/components/landing/HighlightsWidget.vue';
-import PricingWidget from '@/components/landing/PricingWidget.vue';
 import TopbarWidget from '@/components/landing/TopbarWidget.vue';
 </script>
 
@@ -14,9 +11,6 @@ import TopbarWidget from '@/components/landing/TopbarWidget.vue';
                 <TopbarWidget />
             </div>
             <HeroWidget />
-            <FeaturesWidget />
-            <HighlightsWidget />
-            <PricingWidget />
             <FooterWidget />
         </div>
     </div>
