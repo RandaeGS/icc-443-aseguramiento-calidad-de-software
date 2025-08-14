38f963fa6e931a3160552230dfb601c36ea28552
RandaeGS <garciasanchezrd@gmail.com>
2025-08-14 15:37:44 -0400

    feat: implement e2e tests for landing page

--- proyecto-final/frontend/e2e/landing.spec.js
+++ proyecto-final/frontend/e2e/landing.spec.js
@@ -11,5 +11,31 @@ test('get started button exists', async ({ page }) => {
     await page.goto('/');
 
     // Click the get started link.
-    await page.getByRole('button', { name: 'Get Started' }).isVisible();
+    await page.getByRole('link', { name: 'Get Started' }).isVisible();
+});
+
+test('get started redirects to login', async ({ page }) => {
+    await page.goto('/');
+
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get Started' }).click();
+
+    // Wait for keycloak login site
+    await page.waitForURL('**/realms/project/protocol/openid-connect/auth**');
+    await expect(page.getByText('project')).toBeVisible();
+    await expect(page.locator('#username')).toBeVisible();
+});
+
+test('login redirects to products', async ({ page }) => {
+    await page.goto('/');
+
+    // Wait for keycloak login site
+    await page.getByRole('link', { name: 'Get Started' }).click();
+    await page.waitForURL('**/realms/project/protocol/openid-connect/auth**');
+
+    await page.locator('#username').fill('admin');
+    await page.locator('#password').fill('123456');
+
+    await page.locator('#kc-login').click();
+    await page.waitForURL('**/products**');
 });
